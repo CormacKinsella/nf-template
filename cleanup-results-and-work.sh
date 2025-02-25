bf7c871418cce71de9929277cef2539b3ba9cce1
CormacKinsella <27350062+CormacKinsella@users.noreply.github.com>
2025-02-25 16:27:10 +0100

    add safety to rm work

--- cleanup-results-and-work.sh
+++ cleanup-results-and-work.sh
@@ -4,6 +4,9 @@ shopt -s extglob
 
 rm -rf results
 
-cd work
-
-rm -rf  !(apptainer|singularity|conda)
+if [ -d "work" ]; then
+		cd work
+		rm -rf  !(apptainer|singularity|conda)
+else
+	echo "Work directory does not exist."
+fi
